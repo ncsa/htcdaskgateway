7f653a221c1d01832932713f2ce31c297e1101db
Benjamin Galewsky <bengal1@illinois.edu>
2025-07-09 22:44:10 +0000

    Remove scratch.local mount

--- htcdaskgateway/_version.py
+++ htcdaskgateway/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.5.dev1+g64977da.d20250709'
-__version_tuple__ = version_tuple = (1, 0, 5, 'dev1', 'g64977da.d20250709')
+__version__ = version = '1.0.5.dev3+gf1d34f4'
+__version_tuple__ = version_tuple = (1, 0, 5, 'dev3', 'gf1d34f4')
