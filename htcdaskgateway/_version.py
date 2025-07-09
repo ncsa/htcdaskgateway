74f63175c506fff5c34261fbff9de17169b13b86
Benjamin Galewsky <bengal1@illinois.edu>
2025-07-09 19:34:24 +0000

    Fix format of apptainer exec command

--- htcdaskgateway/_version.py
+++ htcdaskgateway/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.2.dev6+g5379e9a.d20250418'
-__version_tuple__ = version_tuple = (1, 0, 2, 'dev6', 'g5379e9a.d20250418')
+__version__ = version = '1.0.5.dev1+g64977da.d20250709'
+__version_tuple__ = version_tuple = (1, 0, 5, 'dev1', 'g64977da.d20250709')
